43fe2febbdfec6ccd4c66b3cdbf54e04420a2971
nikita <nikita@example.com>
2021-07-12 15:14:08 +0300

    bomba

--- site1/models.py
+++ site1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Data(models.Model):
+	units = ArrayField(
+		ArrayField(models.FloatField())
+		)
+	volts = ArrayField(
+		ArrayField(models.FloatField())
+		)
+	freqs = ArrayField(
+		ArrayField(models.FloatField())
+		)
+	mags = ArrayField(
+		ArrayField(models.FloatField())
+		)
+

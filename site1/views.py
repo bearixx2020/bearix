b7aa37ca7cf20080bf922255045061f06648bbca
GubernatorNagibator <you@example.com>
2021-07-14 14:30:24 +0300

    email

--- site1/views.py
+++ site1/views.py
@@ -6,28 +6,23 @@ def get_example(request):
     return render(request,'example/index_example.html')
 
 def get_home(request):
+    if request.method == 'POST' :
+        Name = request.POST.get('Name')
+        LastName = request.POST.get('LastName')
+        Email = request.POST.get('Email')
+        Message = request.POST.get('Message')
+        
+        data = {
+            'name' : Name,
+            'lastName': LastName, 
+            'Email' : Email, 
+            'Message' : Message
+        }
+        send_mail(data['Email'],data['Message'],'',['bearixx2020@gmail.com']) 
+        print(data)
+
     return render(request,'main_window/index_home.html')
 
 def get_uppload_csv(request):
     return render(request,'uppload_csv/index_upload.html')
 
-def post_share(request, post_id):
-    # Retrieve post by id
-    post = get_object_or_404(Post, id=post_id, status='published')
-    sent = False
-    if request.method == 'POST':
-        # Form was submitted
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            # Form fields passed validation
-            cd = form.cleaned_data
-            post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
-            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
-            send_mail(subject, message, 'admin@myblog.com',[cd['to']])
-            sent = True
-    else:
-        form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form,
-                                                    'sent': sent})
